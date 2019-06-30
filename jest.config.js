a018725dd486bc6b1ad161ef8c6024f2ca23f220
Tomasz Janowski <janaz9@gmail.com>
2019-06-30 11:19:38 +1000

    Skip testing Koa integration in Node6

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,16 @@
-module.exports = {
+const config = {
   preset: 'ts-jest',
   testMatch: [
     "<rootDir>/test/**/*.(test|spec).ts",
     "<rootDir>/integration/**/*.(test|spec).ts"
   ]
 };
+
+if (process.env.NODE_6 === 'yes') {
+  // disable Koa integration in node 6
+  config.testPathIgnorePatterns = [
+    "/node_modules/", "/integration-koa/"
+  ];
+}
+
+module.exports = config;
