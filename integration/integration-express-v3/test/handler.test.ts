fced63368851f0b3b230f835c5e2fce221ab5349
Tomasz Janowski <janaz9@gmail.com>
2020-04-27 22:17:31 +1000

    Use async / await in tests

--- integration/integration-express-v3/test/handler.test.ts
+++ integration/integration-express-v3/test/handler.test.ts
@@ -4,17 +4,16 @@ import app = require('../app');
 const H = handler(app as any);
 
 describe('handler function', () => {
-  it('returns 404 if not found', () => {
+  it('returns 404 if not found', async () => {
     const reqOptions = {
       method: 'GET',
       path: '/notfound',
     };
-    return H(reqOptions).then((res) => {
-      expect(res.statusCode).toEqual(404);
-    });
+    const res = await H(reqOptions);
+    expect(res.statusCode).toEqual(404);
   })
 
-  it('parses JSON body', () => {
+  it('parses JSON body', async () => {
     const reqOptions = {
       method: 'GET',
       path: '/reflect',
@@ -23,12 +22,11 @@ describe('handler function', () => {
       },
       body: JSON.stringify({hello: "world"})
     };
-    return H(reqOptions).then((res) => {
-      expect(JSON.parse(res.body.toString()).body).toEqual({"hello": "world"});
-    });
+    const res = await H(reqOptions);
+    expect(JSON.parse(res.body.toString()).body).toEqual({ "hello": "world" });
   })
 
-  it('parses urlencoded body', () => {
+  it('parses urlencoded body', async () => {
     const reqOptions = {
       method: 'POST',
       path: '/reflect',
@@ -37,35 +35,32 @@ describe('handler function', () => {
       },
       body: 'hello=world'
     };
-    return H(reqOptions).then((res) => {
-      expect(JSON.parse(res.body.toString()).body).toEqual({"hello": "world"});
-    });
+    const res = await H(reqOptions);
+    expect(JSON.parse(res.body.toString()).body).toEqual({ "hello": "world" });
   })
 
-  it('returns static file', () => {
+  it('returns static file', async () => {
     const reqOptions = {
       method: 'GET',
       path: '/static/file.txt',
     };
-    return H(reqOptions).then((res) => {
-      expect(res.statusCode).toEqual(200);
-      expect(res.headers['content-type']).toEqual('text/plain; charset=UTF-8');
-      expect(res.body.toString()).toEqual('hello world\n');
-    });
+    const res = await H(reqOptions);
+    expect(res.statusCode).toEqual(200);
+    expect(res.headers['content-type']).toEqual('text/plain; charset=UTF-8');
+    expect(res.body.toString()).toEqual('hello world\n');
   })
 
-  it('returns binary static file', () => {
+  it('returns binary static file', async () => {
     const reqOptions = {
       method: 'GET',
       path: '/static/file.png',
     };
-    return H(reqOptions).then((res) => {
-      expect(res.statusCode).toEqual(200);
-      expect(res.headers['content-type']).toEqual('image/png');
-      expect(res.headers['content-length']).toEqual('178');
-      expect(res.isUTF8).toEqual(false);
-      expect(res.body.toString('base64')).toEqual('iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAA7EAAAOxAGVKw4bAAAAZElEQVQ4jd2TQQqAMAwEpyV6EP//R19gvLReWgwUK0FBcY4hGZbAhgwLMAORlnQxUynH08lyjwiIGOvgFKwAwhE9Bcd1NjFu8Q2B9/vPJ/iB4N0n1i5EYMu9zZaR0kY1Ig8K6A6mlxIIums73gAAAABJRU5ErkJggg==');
-    });
+    const res = await H(reqOptions);
+    expect(res.statusCode).toEqual(200);
+    expect(res.headers['content-type']).toEqual('image/png');
+    expect(res.headers['content-length']).toEqual('178');
+    expect(res.isUTF8).toEqual(false);
+    expect(res.body.toString('base64')).toEqual('iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAA7EAAAOxAGVKw4bAAAAZElEQVQ4jd2TQQqAMAwEpyV6EP//R19gvLReWgwUK0FBcY4hGZbAhgwLMAORlnQxUynH08lyjwiIGOvgFKwAwhE9Bcd1NjFu8Q2B9/vPJ/iB4N0n1i5EYMu9zZaR0kY1Ig8K6A6mlxIIums73gAAAABJRU5ErkJggg==');
   })
 
 })
--- integration/integration-express/test/handler.test.ts
+++ integration/integration-express/test/handler.test.ts
@@ -5,17 +5,16 @@ import zlib from 'zlib';
 const H = handler(app as any);
 
 describe('handler function', () => {
-  it('returns 404 if not found', () => {
+  it('returns 404 if not found', async () => {
     const reqOptions = {
       method: 'GET',
       path: '/notfound',
     };
-    return H(reqOptions).then((res) => {
-      expect(res.statusCode).toEqual(404);
-    });
+    const res = await H(reqOptions);
+    expect(res.statusCode).toEqual(404);
   })
 
-  it('parses JSON body', () => {
+  it('parses JSON body', async () => {
     const reqOptions = {
       method: 'GET',
       path: '/reflect',
@@ -24,33 +23,32 @@ describe('handler function', () => {
       },
       body: JSON.stringify({hello: "world"})
     };
-    return H(reqOptions).then((res) => {
-      expect(JSON.parse(res.body.toString())).toEqual({
-        baseUrl: "/reflect",
-        body:  {
-          hello: "world",
-        },
-        cookies: {},
-        fresh: false,
-        ip: "123.123.123.123",
-        ips: [],
-        method: "GET",
-        originalUrl: "/reflect",
-        params: {},
-        path: "/",
-        protocol: "http",
-        query: {},
-        secure: false,
-        signedCookies: {},
-        stale: true,
-        subdomains: [],
-        url: "/",
-        xhr: false,
-      });
+    const res = await H(reqOptions);
+    expect(JSON.parse(res.body.toString())).toEqual({
+      baseUrl: "/reflect",
+      body: {
+        hello: "world",
+      },
+      cookies: {},
+      fresh: false,
+      ip: "123.123.123.123",
+      ips: [],
+      method: "GET",
+      originalUrl: "/reflect",
+      params: {},
+      path: "/",
+      protocol: "http",
+      query: {},
+      secure: false,
+      signedCookies: {},
+      stale: true,
+      subdomains: [],
+      url: "/",
+      xhr: false,
     });
   })
 
-  it('parses urlencoded body', () => {
+  it('parses urlencoded body', async () => {
     const reqOptions = {
       method: 'POST',
       path: '/reflect',
@@ -59,38 +57,35 @@ describe('handler function', () => {
       },
       body: 'hello=world'
     };
-    return H(reqOptions).then((res) => {
-      expect(JSON.parse(res.body.toString()).body).toEqual({"hello": "world"});
-    });
+    const res = await H(reqOptions);
+    expect(JSON.parse(res.body.toString()).body).toEqual({ "hello": "world" });
   })
 
-  it('returns static file', () => {
+  it('returns static file', async () => {
     const reqOptions = {
       method: 'GET',
       path: '/static/file.txt',
     };
-    return H(reqOptions).then((res) => {
-      expect(res.statusCode).toEqual(200);
-      expect(res.headers['content-type']).toEqual('text/plain; charset=UTF-8');
-      expect(res.body.toString()).toEqual('hello world\n');
-    });
+    const res = await H(reqOptions);
+    expect(res.statusCode).toEqual(200);
+    expect(res.headers['content-type']).toEqual('text/plain; charset=UTF-8');
+    expect(res.body.toString()).toEqual('hello world\n');
   })
 
-  it('returns binary static file', () => {
+  it('returns binary static file', async () => {
     const reqOptions = {
       method: 'GET',
       path: '/static/file.png',
     };
-    return H(reqOptions).then((res) => {
-      expect(res.statusCode).toEqual(200);
-      expect(res.headers['content-type']).toEqual('image/png');
-      expect(res.headers['content-length']).toEqual('178');
-      expect(res.isUTF8).toEqual(false);
-      expect(res.body.toString('base64')).toEqual('iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAA7EAAAOxAGVKw4bAAAAZElEQVQ4jd2TQQqAMAwEpyV6EP//R19gvLReWgwUK0FBcY4hGZbAhgwLMAORlnQxUynH08lyjwiIGOvgFKwAwhE9Bcd1NjFu8Q2B9/vPJ/iB4N0n1i5EYMu9zZaR0kY1Ig8K6A6mlxIIums73gAAAABJRU5ErkJggg==');
-    });
+    const res = await H(reqOptions);
+    expect(res.statusCode).toEqual(200);
+    expect(res.headers['content-type']).toEqual('image/png');
+    expect(res.headers['content-length']).toEqual('178');
+    expect(res.isUTF8).toEqual(false);
+    expect(res.body.toString('base64')).toEqual('iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAA7EAAAOxAGVKw4bAAAAZElEQVQ4jd2TQQqAMAwEpyV6EP//R19gvLReWgwUK0FBcY4hGZbAhgwLMAORlnQxUynH08lyjwiIGOvgFKwAwhE9Bcd1NjFu8Q2B9/vPJ/iB4N0n1i5EYMu9zZaR0kY1Ig8K6A6mlxIIums73gAAAABJRU5ErkJggg==');
   })
 
-  it('works with compressed response', () => {
+  it('works with compressed response', async () => {
     const reqOptions = {
       method: 'GET',
       path: '/static/big.html',
@@ -108,16 +103,13 @@ describe('handler function', () => {
       });
     });
 
-    return H(reqOptions).then((res) => {
-      expect(res.statusCode).toEqual(200);
-      expect(res.headers['content-type']).toEqual('text/html; charset=UTF-8');
-      expect(res.headers['content-encoding']).toEqual('gzip');
-      expect(res.isUTF8).toEqual(false);
-      return gunzip(res.body);
-    })
-    .then(body => {
-      expect(body.toString()).toMatch(/^<!DOCTYPE html>/);
-    })
+    const res = await H(reqOptions);
+    expect(res.statusCode).toEqual(200);
+    expect(res.headers['content-type']).toEqual('text/html; charset=UTF-8');
+    expect(res.headers['content-encoding']).toEqual('gzip');
+    expect(res.isUTF8).toEqual(false);
+    const body = await gunzip(res.body);
+    expect(body.toString()).toMatch(/^<!DOCTYPE html>/);
 
   });
 
